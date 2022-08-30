294df847eca0d24abafcc2e102102bfffb97508e
ISAAC O <onongeisaac@example.com>
2022-08-30 18:56:22 +0300

    quote

--- routers/pages.js
+++ routers/pages.js
@@ -10,6 +10,7 @@ router.get('/pages/campus', (req,res)=>{res.sendFile(path.join(__dirname,'../bui
 router.get('/pages/read', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/devs', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html'))})
 
+router.get('/pages/admin/controls', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 
 
 
