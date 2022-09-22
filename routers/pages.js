d06538c5447e6817fd992096857e947e06e045a2
ISAAC O <onongeisaac@example.com>
2022-09-22 15:22:02 +0300

    blunthomie

--- routers/pages.js
+++ routers/pages.js
@@ -14,9 +14,9 @@ router.get('/pages/services', (req,res)=>{res.sendFile(path.join(__dirname,'../b
 router.get('/pages/admin/requests', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/admin/recommendations', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/admin/kayasers', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
-router.get('/pages/bluntHomie/blunthomie', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
+router.get('/pages/part1/part1home', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/cu/cuhome', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
-router.get('/pages/pant/panthome', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
+router.get('/pages/part2/part2home', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 
 router.get('/pages/admin/controls', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 
