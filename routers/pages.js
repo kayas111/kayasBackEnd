fb101294044a1da9906202c19531133fd52fa918
ISAAC O <onongeisaac@example.com>
2022-09-22 13:42:51 +0300

    blunthomie

--- routers/pages.js
+++ routers/pages.js
@@ -14,6 +14,7 @@ router.get('/pages/services', (req,res)=>{res.sendFile(path.join(__dirname,'../b
 router.get('/pages/admin/requests', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/admin/recommendations', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/admin/kayasers', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
+router.get('/pages/bluntHomie/blunthomie', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/cu/cuhome', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 router.get('/pages/pant/panthome', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
 
