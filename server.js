49e1a0966cd3bf81b88ef81c5da64ac42e2e660a
ISAAC O <onongeisaac@example.com>
2022-12-23 17:29:41 +0300

    updating project for payment

--- server.js
+++ server.js
@@ -1580,7 +1580,7 @@ catch(err){
  
         // It's a good idea to log all received events.
         console.log("..................................webhook received.....................................")
-        console.log(req.res.body)
+        console.log(req)
         
         // Do something (that doesn't take too long) with the payload
         res.status(200).end()
