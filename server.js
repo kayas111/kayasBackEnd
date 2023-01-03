9b297b5056501396361102f9a48ec0e1363a250f
ISAAC O <onongeisaac@example.com>
2023-01-03 22:14:18 +0300

    W

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true}).then(res=
     console.log("Listening on port")
     console.log(port)
     
-    
+
 SendMail("Server launched","onongeisaac@gmail.com","listening on port "+port)
    
     
