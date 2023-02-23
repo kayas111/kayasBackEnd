850a75b8d923b3ef38234af3d6e4723737bb3e53
unknown <kayas.makerere@gmail.com>
2023-02-23 13:56:03 +0300

    f

---
+++ routers/pages.js
@@ -0,0 +1,26 @@
+const express=require('express')
+
+const router =express.Router()
+const path=require('path')
+
+
+router.get('/pages/:page', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html'))}) 
+
+//router.get('/pages/:part/:page', (req,res)=>{res.render(<div>Hello world</div>) })
+
+
+router.get('/pages/:part/:page', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
+router.get('/pages/:part/:subpart/:part', (req,res)=>{res.sendFile(path.join(__dirname,'../build/index.html')) })
+
+module.exports=router
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const path=require('path')
+require('dotenv').config()
+const request = require('request')
+const sgMail=require("@sendgrid/mail")
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const { ReturnDocument } = require('mongodb')
+const bodyParser=require('body-parser')
+const {google}=require('googleapis')
+const nodemailer=require('nodemailer')
+const Flutterwave=require('flutterwave-node-v3')
+const flw = new Flutterwave(process.env.flwPublicKey,process.env.flwSecretKey)
+const emailValidator = require('deep-email-validator');
+const mongoose=require('mongoose')
+mongoose.set('strictQuery', false)
+const bcrypt=require('bcrypt')
+var formidable = require('formidable');
+const dbURI="mongodb+srv://isaac:onongeopio@cluster0.xjf8j.mongodb.net/mydb?retryWrites=true&w=majority"
+const port=process.env.PORT || 4000
+mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true}).then(res=>app.listen(port,()=>{
+    console.log("Listening on port")
+    console.log(port)
+   /* db.collection('multidocs').find({desc:'messagees'}).toArray().then(resp=>{
+        let send=[]
+        resp[0].messagees.forEach(mes=>{
+            send.push('256'+mes)
+        })
+        console.log(send)
+       
+    wbm.start({qrCodeData:true,sessions:true,showBrowser:true}).then(async (rep)=>{
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+        await wbm.waitQRCode()
+        await wbm.send(send,'Hello this is Kayas, I wish to request for a moment of your time, is it okay? .k')
+      
+    
+    }).catch((err)=>{
+        console.log("kayas the error is:")
+        console.log(err)
+    })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-})
+
+    })*/
+
+//SendMail("Kayas Server launched","onongeisaac@gmail.com","listening on port "+port)
+   
+    
+}))
