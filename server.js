19372d097486b88e126bcd768d18732dd25d251c
unknown <kayas.makerere@gmail.com>
2023-03-07 18:02:10 +0300

    h

--- server.js
+++ server.js
@@ -195,6 +195,7 @@ app.get('/collection_controls_visits', (req,res)=>{
        
     
     }) 
+    app.get('/collection_registers_registers', (req,res)=>{db.collection('registers').find().toArray().then((array)=>{res.send(array)})}) 
     app.get('/collections_opinionpolls_cand1', (req,res)=>{db.collection('opinionpolls').find({candidateNumber:1}).toArray().then((array)=>{res.send(array)})}) 
     app.get('/collections_opinionpolls_cand2', (req,res)=>{db.collection('opinionpolls').find({candidateNumber:2}).toArray().then((array)=>{res.send(array)})}) 
     app.get('/collections_opinionpolls_cand3', (req,res)=>{db.collection('opinionpolls').find({candidateNumber:3}).toArray().then((array)=>{res.send(array)})}) 
