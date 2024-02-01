f7de42738b9699f8067eb0a0ffceb06ee21d3d44
unknown <kayas.makerere@gmail.com>
2024-02-02 01:49:27 +0300

    abibi

---
+++ build/service-worker.js
@@ -0,0 +1,25 @@
+
+self.addEventListener('install',e=>{
+    
+  self.skipWaiting()
+    
+})
+self.addEventListener('activate',e=>{
+    
+    clients.claim().then((resp)=>{;})
+})
+
+self.addEventListener('push',e=>{
+let data=e.data.json()
+
+self.registration.showNotification(data.title,{body:data.body,icon:''})
+
+
+})
+
+self.addEventListener(
+  "pushsubscriptionchange",
+  (e) => {
+console.log(e)
+
+  })
